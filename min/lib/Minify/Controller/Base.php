9cfe0878582893693950fab936d081779f8ebebe
Even André Fiskvik <eaf@oyatel.com>
2012-03-23 08:27:29 +0100

    Hidden require_once there. Removing it

--- min/lib/Minify/Controller/Base.php
+++ min/lib/Minify/Controller/Base.php
@@ -244,7 +244,6 @@ abstract class Minify_Controller_Base {
      * @return null
      */
     public function log($msg) {
-        require_once 'Minify/Logger.php';
         Minify_Logger::log($msg);
     }
 }
